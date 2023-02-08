af0c63fc2bb48581034708841749bca77434559b
Simo Ryu <cloneofsimo@gmail.com>
2023-02-08 18:39:35 +0000

    refactor

--- inversion_edits/ddim_inv.py
+++ inversion_edits/ddim_inv.py
@@ -31,29 +31,25 @@ def _preprocess(image: PIL.Image):
     return 2.0 * image - 1.0
 
 
-def _backward_ddim(x_t, alpha_t, alpha_tm1, eps_xt):
-    """from noise to image"""
-    # From https://github.com/cccntu/efficient-prompt-to-prompt/blob/main/ddim-inversion.ipynb.
-    return (
-        alpha_tm1**0.5
-        * (
-            (alpha_t**-0.5 - alpha_tm1**-0.5) * x_t
-            + ((1 / alpha_tm1 - 1) ** 0.5 - (1 / alpha_t - 1) ** 0.5) * eps_xt
-        )
-        + x_t
-    )
+def _backward_ddim(x_tm1, alpha_t, alpha_tm1, eps_xt):
+    """
+    let a = alpha_t, b = alpha_{t - 1}
+    We have a > b,
+    x_{t} - x_{t - 1} = sqrt(a) ((sqrt(1/b) - sqrt(1/a)) * x_{t-1} + (sqrt(1/a - 1) - sqrt(1/b - 1)) * eps_{t-1})
+    From https://arxiv.org/pdf/2105.05233.pdf, section F.
+    """
+   
+    a, b = alpha_t, alpha_tm1
+    sa = a ** 0.5
+    sb = b ** 0.5
+    
+    return sa * ((1/sb - 1/sa) * x_tm1 + ((1/a - 1) ** 0.5 - (1/b - 1) ** 0.5) * eps_xt) + x_tm1
+        
 
+def _prepare(pipe, image, condition_prompt):
 
-@torch.no_grad()
-def ddim_inversion_latent(
-    pipe: StableDiffusionPipeline,
-    image: PIL.Image,
-    num_inference_steps: int = 50,
-    condition_prompt: str = "",
-):
-    if not isinstance(pipe.scheduler, DDIMScheduler):
-        pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
-        print("Scheduler for Pipe is now DDIMScheduler")
+    scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+    print("Scheduler for Pipe is now DDIMScheduler")
 
     vae, unet, text_encoder, tokenizer, scheduler = (
         pipe.vae,
@@ -83,15 +79,29 @@ def ddim_inversion_latent(
 
     latents = x_T.clone()
 
+    return latents, cond_emb, scheduler
+
+
+@torch.no_grad()
+def ddim_inversion_latent(
+    pipe: StableDiffusionPipeline,
+    image: PIL.Image,
+    num_inference_steps: int = 50,
+    condition_prompt: str = "",
+):
+    latents, cond_emb, scheduler = _prepare(pipe, image, condition_prompt)
+
     scheduler.set_timesteps(num_inference_steps)
-    reverse_process = True
+
     for t in tqdm(reversed(scheduler.timesteps)):
         # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
         latent_model_input = latents
 
-        latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
+        # latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
 
-        noise_pred = unet(latent_model_input, t, encoder_hidden_states=cond_emb).sample
+        noise_pred = pipe.unet(
+            latent_model_input, t, encoder_hidden_states=cond_emb
+        ).sample
 
         prev_timestep = (
             t - scheduler.config.num_train_timesteps // scheduler.num_inference_steps
@@ -105,10 +115,19 @@ def ddim_inversion_latent(
         )
 
         latents = _backward_ddim(
-            x_t=latents,
-            alpha_t=alpha_prod_t_prev,
-            alpha_tm1=alpha_prod_t,
+            x_tm1=latents,
+            alpha_t=alpha_prod_t,
+            alpha_tm1=alpha_prod_t_prev,
             eps_xt=noise_pred,
         )
 
     return latents
+
+
+def edict_inversion_latent(
+    pipe: StableDiffusionPipeline,
+    image: PIL.Image,
+    num_inference_steps: int = 50,
+    condition_prompt: str = "",
+):
+    pass
